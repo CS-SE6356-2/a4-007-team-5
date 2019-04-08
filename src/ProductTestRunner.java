42f95cfb27c4ce00edf67a851a74b2822e71148f
TCLVI <tommylea6@gmail.com>
2019-04-08 16:01:35 -0500

    Reformatted project directory to better match the other pushes. - Thomas

--- src/ProductTestRunner.java
+++ src/ProductTestRunner.java
@@ -2,25 +2,24 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-
-
-public class ProductTestRunner {
-
-    public static void main(String[] args){
-
-        Result result = JUnitCore.runClasses(ProductTest.class);
-
-        for(Failure failure : result.getFailures()){
-
+public class ProductTestRunner
+{
+    public static void main(String[] args)
+    {
+        Result result = JUnitCore.runClasses(ShoppingCartTest.class);
+        for (Failure failure : result.getFailures())
+        {
             System.out.println(failure.toString());
         }
 
-        System.out.println(result.wasSuccessful());
-
+        if(result.wasSuccessful())
+        {
+            System.out.println();
+            System.out.println("Testing was successful");
+        }
+        else
+        {
+            System.out.println("Testing produced one or more failures.");
+        }
     }
-
-
-
-
-
 }
